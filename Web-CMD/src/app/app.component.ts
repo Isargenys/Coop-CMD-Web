307eeb7808f6513ce99f89649c60fa97ab95cbac
Isargenys Contreras <48723570+Isargenys@users.noreply.github.com>
2023-03-20 23:41:05 -0400

    cambios en el home

--- Web-CMD/src/app/app.component.ts
+++ Web-CMD/src/app/app.component.ts
@@ -10,8 +10,8 @@ import { AuthService } from './auth/auth-guard.service';
 
     <div class="d-flex" id="wrapper">
         
-        <div class="border-end bg-white" id="sidebar-wrapper">
-            <div class="sidebar-heading border-bottom bg-light text-center">Coop-CMD</div>
+        <div class="border-end " id="sidebar-wrapper">
+            <div class="empresaName sidebar-heading border-bottom  text-center text-white">Coop-CMD</div>
             <div class="list-group list-group-flush">
                 <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">
                     <i class="bi bi-house-door"></i> Inicio
@@ -37,20 +37,23 @@ import { AuthService } from './auth/auth-guard.service';
         </div>
         
         <div id="page-content-wrapper">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <nav class="navbar navbar-expand-lg navbar-light  border-bottom">
                 <div class="container-fluid">
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                            <li class="nav-item"><a class="nav-link btn btn-primary" href="#!">
-                                <i class="bi bi-bell"></i>
+                            <li class="nav-item"><a class="nav-link btn text-white" href="#!">
+                                <i class="bi bi-bell-fill"></i>
                             </a></li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="bi bi-person-fill"></i> Nombre Usuario</a>
+                            <li class="nav-item dropdown ">
+                                <a class="nav-link dropdown-toggle text-white" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="bi bi-person-fill"></i>
+                                 Nombre Usuario
+                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#!">Action</a>
-                                    <a class="dropdown-item" href="#!">Another action</a>
+                                    <a class="dropdown-item" href="#!">Perfil</a>
+                                    <a class="dropdown-item" href="#!">Configuraciones</a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#!">Something else here</a>
+                                    <a class="dropdown-item" href="#!">Salir del sistema</a>
                                 </div>
                             </li>
                         </ul>
